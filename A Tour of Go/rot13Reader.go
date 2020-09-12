a6be9fc2afaedacebc00045fbe0a87a644293acd
vonxar <p02d1108@gmail.com>
2020-09-12 21:05:20 +0900

    go勉強_インター

---
+++ A Tour of Go/rot13Reader.go
@@ -0,0 +1,32 @@
+//全然理解できない
+
+package main
+
+import (
+	"io"
+	"os"
+	"strings"
+)
+
+type rot13Reader struct {
+	r io.Reader
+}
+
+func (rot13 rot13Reader) Read(b []byte) (int, error) {
+	n, err := rot13.r.Read(b)
+	for i, v := range b {
+		switch {
+		case v >= 'A' && v < 'N', v >= 'a' && v < 'n':
+			b[i] += 13
+		case v >= 'N' && v <= 'Z', v >= 'n' && v <= 'z':
+			b[i] -= 13
+		}
+	}
+	return n, err
+}
+
+func main() {
+	s := strings.NewReader("Lbh penpxrq gur pbqr!")
+	r := rot13Reader{s}
+	io.Copy(os.Stdout, &r)
+}
