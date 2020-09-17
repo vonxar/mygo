05240df27b6d9a7a58993a5c43cab3a394bd61c1
vonxar <p02d1108@gmail.com>
2020-09-17 21:40:02 +0900

    Go6章

--- GoBook/3/main.go
+++ GoBook/3/main.go
@@ -1,17 +1,20 @@
 package main
 
 import (
-	"math/rand"
-	"net/http"
-	"text/template"
-	"time"
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
 )
 
 // 送信構造体
-// type Post struct {
-// 	User    string
-// 	Threads []string
-// }
+type Post struct {
+	// User    string
+	// Threads []string
+	Id      int
+	Content string
+	Author  string
+}
 
 // 書き込み
 // func writerExample(w http.ResponseWriter, r *http.Request) {
@@ -129,29 +132,194 @@ import (
 // 	return t.Format(layout)
 // }
 
-func process(w http.ResponseWriter, r *http.Request) {
-	rand.Seed(time.Now().Unix())
-	var t *template.Template
-	if rand.Intn(10) > 5 {
-		t, _ = template.ParseFiles("layout.html", "red.html")
-	} else {
-		t, _ = template.ParseFiles("layout.html")
+// func process(w http.ResponseWriter, r *http.Request) {
+// 	rand.Seed(time.Now().Unix())
+// 	var t *template.Template
+// 	if rand.Intn(10) > 5 {
+// 		t, _ = template.ParseFiles("layout.html", "red.html")
+// 	} else {
+// 		t, _ = template.ParseFiles("layout.html")
+// 	}
+// 	t.ExecuteTemplate(w, "layout", "")
+// }
+
+// func form(w http.ResponseWriter, r *http.Request) {
+// 	t, _ := template.ParseFiles("form.html")
+// 	t.Execute(w, nil)
+// }
+
+// var PostById map[int]*Post
+// var PostByAuthor map[string][]*Post
+
+// func store(data interface{}, filename string) {
+// 	buffer := new(bytes.Buffer)
+// 	encoder := gob.NewEncoder(buffer)
+// 	err := encoder.Encode(data)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	err = ioutil.WriteFile(filename, buffer.Bytes(), 0600)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// }
+
+// func load(data interface{}, filename string) {
+// 	raw, err := ioutil.ReadFile(filename)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	buffer := bytes.NewBuffer(raw)
+// 	dec := gob.NewDecoder(buffer)
+// 	err = dec.Decode(data)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// }
+
+var Db *sql.DB
+
+func init() {
+	var err error
+	Db, err = sql.Open("postgres", "user=gwp dbname=gwp password=gwp sslmode=disable")
+	if err != nil {
+		panic(err)
 	}
-	t.ExecuteTemplate(w, "layout", "")
 }
 
-func form(w http.ResponseWriter, r *http.Request) {
-	t, _ := template.ParseFiles("form.html")
-	t.Execute(w, nil)
+func Posts(limit int) (posts []Post, err error) {
+	rows, err := Db.Query("select id, content, author from posts limit$1, limit")
+	if err != nil {
+		return
+	}
+	for rows.Next() {
+		post := Post{}
+		err = rows.Scan(&post.Id, &post.Content, &post.Author)
+		if err != nil {
+			return
+		}
+		posts = append(posts, post)
+	}
+	rows.Close()
+	return
+}
+
+func GetPost(id int) (post Post, err error) {
+	post = Post{}
+	err = Db.QueryRow("select id, content,author from posts where id = $1, id").Scan(&post.Id, &post.Content, &post.Author)
+	return
+}
+
+func (post *Post) Create() (err error) {
+	statement := "insert into posts (content, author) valuse ($1, &2) returning id"
+	stmt, err := Db.Prepare(statement)
+	if err != nil {
+		return
+	}
+	defer stmt.Close()
+	err = stmt.QueryRow(post.Content, post.Author).Scan(&post.Id)
+	return
+}
+
+func (post *Post) Update() (err error) {
+	_, err = Db.Exec("update posts set content = $2, author = $3 whereid = $1", post.Id, post.Content, post.Author)
+	return
+}
+
+func (post *Post) Delete() (err error) {
+	_, err = Db.Exec("delete from posts where id = $1", post.Id)
+	return
 }
 
 func main() {
+	post := Post{Content: "Helllo world", Author: "Sau Sheong"}
+	fmt.Println(post)
+	post.Create()
+	fmt.Println(post)
+	readPost, _ := GetPost(post.Id)
+	fmt.Println(readPost)
+
+	readPost.Content = "Bonjour Monde"
+	readPost.Author = "Pierre"
+	readPost.Update()
+	posts, _ := Posts(10)
+	fmt.Println(posts)
+
+	readPost.Delete()
+	// csvFile, err := os.Create("posts.csv")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer csvFile.Close()
+
+	// allPosts := []Post{
+	// 	Post{Id: 1, Content: "Hello world", Author: "Sau Sheong"},
+	// 	Post{Id: 2, Content: "Bonjpur Monde", Author: "Prierre"},
+	// 	Post{Id: 3, Content: "Hello Mundo", Author: "Pedro"},
+	// 	Post{Id: 4, Content: "Greetings Earthlings", Author: "Sau Sheong"},
+	// }
+
+	// writer := csv.NewWriter(csvFile)
+	// for _, post := range allPosts {
+	// 	line := []string{strconv.Itoa(post.Id), post.Content, post.Author}
+	// 	err := writer.Write(line)
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+	// }
+	// writer.Flush()
+
+	// file, err := os.Open("posts.csv")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer file.Close()
+
+	// reader := csv.NewReader(file)
+	// reader.FieldsPerRecord = -1
+	// record, err := reader.ReadAll()
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// var posts []Post
+	// for _, item := range record {
+	// 	id, _ := strconv.ParseInt(item[0], 0, 0)
+	// 	post := Post{Id: int(id), Content: item[1], Author: item[2]}
+	// 	posts = append(posts, post)
+	// }
+
+	// fmt.Println(posts[0].Id)
+	// fmt.Println(posts[0].Content)
+	// fmt.Println(posts[0].Author)
+	// PostById = make(map[int]*Post)
+	// PostByAuthor = make(map[string][]*Post)
+
+	// post1 := Post{Id: 1, Content: "Hello world", Author: "Sau Sheong"}
+	// post2 := Post{Id: 2, Content: "Bonjpur Monde", Author: "Prierre"}
+	// post3 := Post{Id: 3, Content: "Hello Mundo", Author: "Pedro"}
+	// post4 := Post{Id: 4, Content: "Greetings Earthlings", Author: "Sau Sheong"}
+
+	// store(post1)
+	// store(post2)
+	// store(post3)
+	// store(post4)
+
+	// fmt.Println(PostById[1])
+	// fmt.Println(PostById[2])
+
+	// for _, post := range PostByAuthor["Sau Sheong"] {
+	// 	fmt.Println(post)
+	// }
+	// for _, post := range PostByAuthor["Pedro"] {
+	// 	fmt.Println(post)
+	// }
+
 	// mux := httprouter.New()
 	// mux.GET("/hello/:name", hello)
-	server := http.Server{
-		Addr: "127.0.0.1:8080",
-		// Handler: mux,
-	}
+	// server := http.Server{
+	// Addr: "127.0.0.1:8080",
+	// Handler: mux,
+	// }
 	// http.HandleFunc("/write", writerExample)
 	// http.HandleFunc("/writeheandler", writeHeandlerExample)
 	// http.HandleFunc("/redirect", headlerExample)
@@ -160,7 +328,28 @@ func main() {
 	// http.HandleFunc("/get_cookie", getCookie)
 	// http.HandleFunc("/set_message", setMessage)
 	// http.HandleFunc("/show_message", showMessage)
-	http.HandleFunc("/process", process)
-	http.HandleFunc("/form", form)
-	server.ListenAndServe()
+	// http.HandleFunc("/process", process)
+	// http.HandleFunc("/form", form)
+	// server.ListenAndServe()
+
+	// data := []byte("hello world\n")
+	// err := ioutil.WriteFile("datal1", data, 0644)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// read1, _ := ioutil.ReadFile("data1")
+	// fmt.Print(string(read1))
+
+	// file1, _ := os.Create("data2")
+	// defer file1.Close()
+	// bytes, _ := file1.Write(data)
+	// fmt.Printf("Wrote %d bytes to file\n", bytes)
+
+	// file2, _ := os.Open("data2")
+	// defer file2.Close()
+
+	// read2 := make([]byte, len(data))
+	// bytes, _ = file2.Read(read2)
+	// fmt.Printf("REad %d bytes from file\n", bytes)
+	// fmt.Println(string(read2))
 }
